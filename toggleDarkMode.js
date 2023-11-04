738c4bbbc3dc93f6eb4dc747bf49a7e6b3b0cf82
JoaoGuilherme7 <119643173+JoaoGuilherme7@users.noreply.github.com>
2023-11-03 23:46:41 -0300

    Organizando arquivos e códigos e adicionando dark/light mode

---
+++ toggleDarkMode.js
@@ -0,0 +1,50 @@
+const checkDarkMode = select('#light-dark');
+const head = select('head');
+const hour = new Date().getHours();
+
+const darkFields = [
+    ...document.querySelectorAll('section'),
+    select('body')
+];
+
+function select(selector) {
+    return document.querySelector(selector);
+}
+
+function addDarkMode() {
+    darkFields.forEach(field => {
+        field.classList.add('darkMode');
+    });
+}
+
+function removeDarkMode() {
+    darkFields.forEach(field => {
+        field.classList.remove('darkMode');
+    });
+
+}
+
+function toggleDarkMode() {
+    if (checkDarkMode.checked) {
+        select('.light-dark label div').innerHTML = `<i class="fa-regular fa-moon"></i>`;
+        addDarkMode();
+    }
+    else {
+        select('.light-dark label div').innerHTML = `<i class="fa-regular fa-sun"></i>`;
+        removeDarkMode();
+    }
+}
+
+function darkModePerHour() {
+    if (hour >= 18 || hour < 7) {
+        checkDarkMode.checked = true;
+        toggleDarkMode();
+
+    }
+    else if (hour < 18 || hour > 7) {
+        checkDarkMode.checked = false;
+        toggleDarkMode();
+    }
+} darkModePerHour();
+
+checkDarkMode.addEventListener("change", toggleDarkMode);
